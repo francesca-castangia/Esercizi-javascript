3d71d09b2d66fcf68fb7f62e685185c4eb832176
francesca.castangia <castangiaf2@gmail.com>
2025-05-29 19:51:43 +0200

    manipolazione del dom 2

--- Manipolazione-del-DOM-2/script.js
+++ Manipolazione-del-DOM-2/script.js
@@ -2,3 +2,21 @@
 
 // Recuperare il valore di ciascun campo di input e creare un oggetto person contenente le proprietà: firstName, lastName e age.
 // Infine recuperare l'elemento forme aggiungere l'attributo data-person contenente l'oggetto person in formato json. 
+
+const firstName = document.querySelector('#firstName');
+const lastName = document.querySelector('#lastName');
+const age = document.querySelector('#age');
+
+const person = {
+    firstName : firstName.value.toLowerCase(),
+    lastName : lastName.value.toLowerCase(),
+    age : age.value
+}; 
+
+
+
+const form = document.querySelector('form');
+form.setAttribute('data-person', JSON.stringify(person));
+
+
+
