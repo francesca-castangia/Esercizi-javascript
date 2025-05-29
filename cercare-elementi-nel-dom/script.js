6af992e2de7d68a5c72f44c4931d056c50e7c308
francesca.castangia <castangiaf2@gmail.com>
2025-05-29 15:52:05 +0200

    caricato cercare elementi nel dom

---
+++ cercare-elementi-nel-dom/script.js
@@ -0,0 +1,6 @@
+ const p = document.getElementsByClassName('paragrafo');
+        console.log(p);
+        const elemento3 = document.getElementById('elemento-3');
+        console.log(elemento3);
+        const listaElemnti = document.querySelectorAll('h2');
+        console.log(listaElemnti);
